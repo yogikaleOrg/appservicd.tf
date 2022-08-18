48ab844e323d59d0239521f8849bfafc8540d6e3
kumarkasula <106813293+kumarkasula@users.noreply.github.com>
2022-08-18 14:00:47 +0530

    Update Appservice.tf

--- Appservice.tf
+++ Appservice.tf
@@ -7,17 +7,42 @@ resource "azurerm_app_service_plan" "appserviceplan" {
   location            = "eastus" 
   resource_group_name = "DEV-TF-RG" 
 
-  sku {
+  kind = "Linux"
+
+ sku {
     tier = "Standard"
     size = "S1"
   }
+
+  properties {
+    reserved = true # Mandatory for Linux plans
+  }
 }
+
 resource "azurerm_app_service" "appservice_dataservice"{
   name                = "data-appservice"
   location            =  "eastus"
   resource_group_name =  "DEV-TF-RG"
   app_service_plan_id =  azurerm_app_service_plan.appserviceplan.id
-  site_config {}
+  
+  app_settings {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+
+    /*
+    # Settings for private Container Registires  
+    DOCKER_REGISTRY_SERVER_URL      = ""
+    DOCKER_REGISTRY_SERVER_USERNAME = ""
+    DOCKER_REGISTRY_SERVER_PASSWORD = ""
+    */
+  }
+site_config {
+    linux_fx_version = "DOCKER|appsvcsample/static-site:latest"
+    always_on        = "true"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
 resource "azurerm_app_service_source_control" "appservice" {
   app_id   = azurerm_app_service.appservice_dataservice.id
