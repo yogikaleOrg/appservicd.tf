88308981d4196b0670c3d8405a1102c671313fbf
YogeshKale <104999673+yogikaleOrg@users.noreply.github.com>
2024-09-20 19:22:35 +0530

    Update and rename Appservice.tf to RG.tf

---
+++ RG.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+  features {}
+}
+# Create resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.project}-${var.environment}-rg"
+  location = var.location
+  tags = local.tags
+}
